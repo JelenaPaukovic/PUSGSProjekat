d891d64eb4c67aa4d591fbabcf81b4c1823c5737
biljana-bajic <biljanabjc@gmail.com>
2020-05-15 23:28:23 +0200

    Zadnja strana; Kontroleri: Aviokompanija, Letovi; Baza

---
+++ PUSGSProjekat2/ProjekatPUSGS/ProjekatPUSGS/Models/Aviokompanija.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ProjekatPUSGS.Models
+{
+    public class Aviokompanija
+    {
+        [Key]
+        public int IdAvio { get; set; }
+      
+        public string Naziv { get; set; }
+        public string Adresa { get; set; }
+        public string PromotivniOpis { get; set; }
+        public string Destinacije { get; set; }
+        public string Letovi { get; set;}
+        public string SpisakKarataSaPopustom { get; set; }
+        public string KonfSegmenataIMesta { get; set; }
+        public string CenovnikIInfoOPrtljagu { get; set; }
+        
+    }
+}
